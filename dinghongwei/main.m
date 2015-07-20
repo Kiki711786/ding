3f278a591b511235b73acda4804688dd6243bcf4
Kiki <605745868@qq.com>
2015-07-20 10:15:38 +0800

    Initial Commit

---
+++ dinghongwei/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  dinghongwei
+//
+//  Created by qianfeng on 15-7-20.
+//  Copyright (c) 2015年 wwq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
